df05adf5b6405ff714000e226614bb23d03253f4
jackie <jacquelyn.cheng@me.com>
2018-11-05 13:02:44 -0800

    Added tests and relationships/validations for rental model

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,14 @@
 class Rental < ApplicationRecord
   belongs_to :customer
   belongs_to :movie
+
+  validates :checkout_date, presence: true
+  validates :due_date, presence: true
+  validate :due_date_after_checkout_date?
+
+  def due_date_after_checkout_date?
+    if due_date && checkout_date && due_date < checkout_date
+      errors.add :due_date, "must be after checkout date"
+    end
+  end
 end
--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,9 +1,53 @@
 require "test_helper"
 
 describe Rental do
-  let(:rental) { Rental.new }
+  let(:rental) { rentals(:rental1) }
 
   it "must be valid" do
     value(rental).must_be :valid?
+  end
+
+  describe "relations" do
+    it "belongs to a customer" do
+      expect(rental).must_respond_to :customer
+      expect(rental.customer).must_be_kind_of Customer
+    end
+
+    it "belongs to a movie" do
+      expect(rental).must_respond_to :movie
+      expect(rental.movie).must_be_kind_of Movie
+    end
+  end
+
+  describe "validations" do
+    it "must have a checkout_date" do
+      valid = rental.valid?
+      expect(valid).must_equal true
+
+      rental.checkout_date = nil
+      rental.save
+      valid = rental.valid?
+      expect(valid).must_equal false
+    end
+
+    it "must have a due_date" do
+      valid = rental.valid?
+      expect(valid).must_equal true
+
+      rental.due_date = nil
+      rental.save
+      valid = rental.valid?
+      expect(valid).must_equal false
+    end
+
+    it "must have a due_date that is after the checkout_date" do
+      valid = rental.valid?
+      expect(valid).must_equal true
+
+      rental.due_date = rental.checkout_date - 1
+      rental.save
+      valid = rental.valid?
+      expect(valid).must_equal false
+    end
   end
 end
