9f7bd5c6b9bacec923de23130fc7fe8fed9ec54f
jackie <jacquelyn.cheng@me.com>
2018-11-05 13:13:15 -0800

    created customers and movies controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'movies/index'
+  get 'movies/show'
+  get 'movies/create'
+  get 'customers/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/zomg", to: "customers#zomg", as: "zomg"
   
