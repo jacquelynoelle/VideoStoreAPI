12d74c2bc777c4ad38e5a9fcc8fb186b6ab66646
jackie <jacquelyn.cheng@me.com>
2018-11-05 17:09:06 -0800

    rental controller tweaks, switching branches

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -3,17 +3,16 @@ require 'date'
 class RentalsController < ApplicationController
   def checkout
     rental = Rental.new(rental_params)
-    rental.checkout_date = Date.today
-    rental.due_date = Date.today + 7
+    rental.checkout
+    # rental.checkout_date = Date.today
+    # rental.due_date = Date.today + 7
+    # update available inventory
+    # update customer movies checked out
 
     if rental.movie.checkout && rental.save
-      render json: { ok: true,
-        id: rental[:id].as_json
-      }, status: :ok
+      render json: rental.as_json(only: [:id]), status: :ok
     else
-      render json: { ok: false,
-        errors: rental.errors.messages
-      }, status: :bad_request
+      render json: { errors: rental.errors.messages }, status: :bad_request
     end
   end
 
@@ -21,14 +20,13 @@ class RentalsController < ApplicationController
     rental = Rental.find_by(movie_id: params[:movie_id], customer_id: params[:customer_id])
 
     if rental.nil?
-      render json: { ok: false, message: "Rental not found" }, status: :not_found
-    elsif rental.movie.checkin
+      render json: { message: "Rental not found" }, status: :not_found
+    elsif rental.checkin
       render json: {
-        ok: true,
         rental: movie.as_json(except: [:created_at, :updated_at])
-      }, status: :ok
+      }, status: :ok # edit this
     else
-      render json: { ok: false, message: "Could not check-in movie" }, status: :bad_request
+      render json: { message: "Could not check-in movie" }, status: :bad_request
     end
   end
 
