4bc425737e94f9ab30f943f994492a08e3c249b6
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-11-06 15:55:47 -0800

    movies history route added

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get "/movies", to: "movies#index", as: "movies"
   get "/movies/:id", to: "movies#show", as: "movie"
+  get "/movies/:id/history", to: "movies#history", as: "rental_history"
   post "/movies", to: "movies#create"
 
   get "/rentals/overdue", to: "rentals#overdue", as: "overdues"
